2f5079494ae4506d4ee90e3c2ed1b2ac008f2208
save91 <saveriotosi@gmail.com>
2016-01-30 18:18:33 +0100

    Aggiornamento refresh beacons

--- app/www/js/controllers/beacons.js
+++ app/www/js/controllers/beacons.js
@@ -1,8 +1,13 @@
 angular.module('app.controllers.beacons', [])
 
 .controller('BeaconsCtrl', function($scope, Beacons) {
-    $scope.beacons = Beacons.all();
-    $scope.stato = Beacons.stato();
+  $scope.beacons = [];
+  $scope.stato = {};
+    $scope.$on('$ionicView.enter',function(){
+      $scope.beacons = Beacons.all();
+      $scope.stato = Beacons.stato();
+    });
+
     $scope.aggiorna = function() {
         $scope.beacons = Beacons.aggiorna();
     }
--- app/www/js/services/beacons.js
+++ app/www/js/services/beacons.js
@@ -3,19 +3,20 @@ angular.module('todo.services.beacons',[])
 .factory('Beacons', function($http, $ionicPopup) {
 
 	var beacons = [];
-  var stato = {messaggio: 'Ricerca...'};
+  var stato = {messaggio: ''};
   var aggiornamento = function() {
   beacons = [];
+	stato.messaggio = 'Ricerca in corso';
   $http({
         method: 'GET',
-        url: 'http://192.168.1.148/stage/beacons.php'
+        url: 'http://192.168.0.51/stage/beacons.php'
       }).then(function(response) {
         for(var i = 0;i < response.data.length;i++)
         {
           beacons.push(response.data[i]);
         }
         stato.messaggio = '';
-        
+
       }, function(response) {
         var alertPopup = $ionicPopup.alert({
           title: 'ERRORE!',
@@ -26,9 +27,10 @@ angular.module('todo.services.beacons',[])
       });
       return beacons;
     };
-  
+
   return {
   	all: function() {
+			aggiornamento();
   		return beacons;
   	},
     stato: function() {
@@ -47,12 +49,12 @@ angular.module('todo.services.beacons',[])
       };
       $http({
         method: 'GET',
-        url: 'http://192.168.1.148/stage/beacon.php?id=' + elem
+        url: 'http://192.168.0.51/stage/beacon.php?id=' + elem
       }).then(function(response) {
         beacon.id = response.data.id;
         beacon.nome = response.data.nome;
         beacon.descrizione = response.data.descrizione;
-        beacon.url = response.data.url        
+        beacon.url = response.data.url
       }, function(response) {
         var alertPopup = $ionicPopup.alert({
           title: 'ERRORE!',
