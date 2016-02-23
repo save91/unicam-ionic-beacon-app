75194499fa3822f5c47863f5915eb9a10bf6417d
Saverio Tosi <saveriotosi@gmail.com>
2016-02-23 11:15:56 +0100

    Segnalazione iBeacon

--- app/www/js/controllers/cerca.js
+++ app/www/js/controllers/cerca.js
@@ -9,21 +9,39 @@ angular.module('app.controllers.cerca', [])
       $rootScope.$on("$cordovaBeacon:didRangeBeaconsInRegion", function(event, pluginResult) {
           var uniqueBeaconKey;
           for(var i = 0; i < pluginResult.beacons.length; i++) {
-              uniqueBeaconKey = pluginResult.beacons[i].uuid + ":" + pluginResult.beacons[i].major + ":" + pluginResult.beacons[i].minor;
-              $scope.beacons[uniqueBeaconKey] = pluginResult.beacons[i];
+            uniqueBeaconKey = pluginResult.beacons[i].uuid + ":" + pluginResult.beacons[i].major + ":" + pluginResult.beacons[i].minor;
+            if(!$scope.beacons[uniqueBeaconKey]) {
+              $scope.beacons[uniqueBeaconKey] = {};
+            }
+            $scope.beacons[uniqueBeaconKey].uuid = pluginResult.beacons[i].uuid;
+            $scope.beacons[uniqueBeaconKey].major = pluginResult.beacons[i].major;
+            $scope.beacons[uniqueBeaconKey].minor = pluginResult.beacons[i].minor;
+            $scope.beacons[uniqueBeaconKey].proximity = pluginResult.beacons[i].proximity;
+            $scope.beacons[uniqueBeaconKey].accuracy = pluginResult.beacons[i].accuracy;
           }
           $scope.$apply();
       });
       $cordovaBeacon.startRangingBeaconsInRegion($cordovaBeacon.createBeaconRegion("BlueUp", "ACFD065E-C3C0-11E3-9BBE-1A514932AC01"));
   });
-  var callback = function(data) {
-    $ionicPopup.alert({
-      title: "CAricameneto",
-      template: "completato"
-    });
-  }
 
-  $scope.aggiungi = function(major, minor) {
-    Beacons.aggiungi(major, minor).then(callback);
+  $scope.$on('$ionicView.enter',function(){
+    Beacons.all().then(callbackBeacon);
+  });
+  var callbackBeacon = function(data) {
+    var uniqueBeaconKey;
+    for(var i = 0; i < data.length; i++) {
+      uniqueBeaconKey = data[i].uuid + ":" + data[i].major + ":" + data[i].minor;
+      if(!$scope.beacons[uniqueBeaconKey]) {
+        $scope.beacons[uniqueBeaconKey] = {};
+      }
+      $scope.beacons[uniqueBeaconKey].uuid = data[i].uuid;
+      $scope.beacons[uniqueBeaconKey].major = data[i].major;
+      $scope.beacons[uniqueBeaconKey].minor = data[i].minor;
+      $scope.beacons[uniqueBeaconKey].stato = data[i].stato;
+    }
+  };
+
+  $scope.aggiungi = function(uuid, major, minor) {
+    Beacons.aggiungi(uuid, major, minor).then(callbackBeacon);
   }
 })
--- app/www/js/services/ibeacons.js
+++ app/www/js/services/ibeacons.js
@@ -2,15 +2,22 @@ angular.module('app.services.ibeacons',[])
 
 .factory('Beacons', function($http, MY_SERVER) {
   return {
-    aggiungi: function(major, minor) {
+    aggiungi: function(uuid, major, minor) {
         return $http.post(MY_SERVER.get() + '/aggiungi_beacon',
           {
+            uuid: uuid,
             major: major,
             minor: minor
           }
         ).then(function(response) {
           return response.data;
           });
-        }
-      };
+        },
+    all: function() {
+      return $http.get(MY_SERVER.get() + '/beacons')
+        .then(function(response) {
+          return response.data;
+        });
+      }
+    };
 });
